ddc2e090b5698955aee6478fe06910079ef4ebea
Dilawar Singh <dilawars@ncbs.res.in>
2015-09-25 22:54:38 +0530

    This version works with given python script.

--- python/eyeBlinkDataSave.py
+++ python/eyeBlinkDataSave.py
@@ -31,18 +31,9 @@ import os
 import sys
 import time
 import serial
-from collections import defaultdict
 import datetime
-import warnings
 import argparse
 import re
-
-import logging
-logging.basicConfig(level=logging.DEBUG,
-    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-    datefmt='%m-%d %H:%M',
-    filename='eye_blink.log',
-    filemode='w')
 
 # Global argument.
 args_ = {}
@@ -50,42 +41,93 @@ args_ = {}
 class ExpState():
 
     def __init__(self):
-        self.mouseName = None
-        self.session = []
-        self.sessionNo = None
-        self.state = None
-        self.trialNo = None
-    
-    def current_state(self):
-        """get the current state"""
-        if self.mouseName and self.session:
-            self.state = "WAITING_FOR_DATA"
-        elif self.mouseName:
-            self.state = "WAITING_FOR_SESSION_INFO"
+        self.states = {}
+        self.params = {}
+        self.state = 0
+
+    def get_state(self):
+        if self.state < 10:
+            return "WAITING_FOR_MOUSE_INFO"
+        elif self.state < 20:
+            return "WAITING_FOR_SESSION_INFO"
+        elif self.state < 30:
+            return "WAITING_FOR_TRIAL_START"
+        elif self.state < 40:
+            return "WAITING_FOR_TRIAL_END"
+        elif self.state < 50:
+            return "START_WRITE_TRIAL_FILE"
         else:
-            self.state = "WAITING_FOR_MOUSE_INFO"
-        return self.state
+            return "UNKNOWN_STATE"
+
+    def __str__(self):
+        return "%s" % self.get_state()
+
+    def update_state(self, line):
+        global args_
+        if self.get_state() == "WAITING_FOR_MOUSE_INFO":
+            mousePat = re.compile(r'Mouse(?P<name>\w\d)')
+            m = mousePat.search(line)
+            if m:
+                self.params['mouse_name'] = m.group('name')
+                inform_user("\nGot mouse : %s" % self.params['mouse_name'])
+                self.state = 10
+                return None
+            else:
+                print('.', end='')
+                sys.stdout.flush()
+
+        if self.get_state() == "WAITING_FOR_SESSION_INFO":
+            sesPat = re.compile(
+                    r'Session(?P<sno>\d+)\:\s*(?P<stype>(Cntrl|Delay|Trace))'
+                    )
+            m = sesPat.search(line)
+            if m:
+                self.params['session'] = [m.group('sno'), m.group('stype')]
+                inform_user("Session: %s" % self.params['session'])
+                self.state = 20
+                return None
+
+        if self.get_state() == "WAITING_FOR_TRIAL_START":
+            dataPat = re.compile(
+                    r'^\@Trial\s*No\.\s*(?P<tn>\d+)\s*\:\s*(?P<tt>(CS+|CS-))'
+                    )
+            m = dataPat.search(line)
+            if m:
+                inform_user("Trial: %s starts .." % m.group(0))
+                self.params['trial'] = [ m.group('tn'), m.group('tt') ]
+                self.params['trial_file'] = os.path.join(
+                        args_['data_dir']
+                        , "Trial" + "_".join(self.params['trial']) + ".csv"
+                        )
+                # In lieu of current line, add a header to csv file. We need to
+                # write all data starting next line on. We achieve this by
+                # setting the sate value after writing this line.
+                write_to_trial_file("timestamp,data\n")
+                if self.state == 30:
+                    write_to_trial_file(line)
+                self.state = 30
+
+        if self.get_state() == "WAITING_FOR_TRIAL_END":
+            endPat = re.compile(r'Blink\s*Count\s*=\s*(?P<num>\d+)')
+            m = endPat.search(line)
+            if m:
+                inform_user("Trial Ends. Total blinks: %s" % m.group('num'))
+                inform_user("Wrote trial file: %s" % self.params['trial_file'])
+                self.state = 20
+
+    def write_to_trial_file(self, line):
+        global args_
+        trialFile = os.path.join(
+                args_['data_dir']
+                , "_".join(self.params['trial'])
+                )
+        with open(trialFile, "a") as f:
+            f.write("%s\n" % line)
+
+    def insert_line(self, line):
+        self.update_state(line)
 
 exp_ = ExpState()
-
-# define a Handler which writes INFO messages or higher to the sys.stderr
-console = logging.StreamHandler()
-console.setLevel(logging.INFO)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-console.setFormatter(formatter)
-# add the handler to the root logger
-_logger = logging.getLogger('')
-_logger.addHandler(console)
-
-DATA_BEGIN_MARKER = "["
-DATA_END_MARKER = "]"
-COMMENT_MARKER = "#"
-TRIAL_DATA_MARKER = "@"
-PROFILING_DATA_MARKER = "$"
-START_OF_SESSION_MARKER = "<"
-END_OF_SESSION_MARKER = ">"
 
 def inform_user(msg):
     info = None
@@ -107,11 +149,12 @@ def get_default_ports():
         quit()
     return ports
 
-def getSerialPort(portPath, baudRate = 9600, timeout = 1):
+def get_serial_port(portPath, baudRate = 9600, timeout = 1):
     """If portPath is given, connet to it. Else try few default ports"""
-    ports = [ ]
+
+    import serial.tools.list_ports as lp
     if not portPath:
-        ports = get_default_ports()
+        ports = sorted([ x for x, y, z in lp.comports() ])
     else:
         ports = [ portPath ]
 
@@ -119,7 +162,7 @@ def getSerialPort(portPath, baudRate = 9600, timeout = 1):
     for i, p in enumerate(ports):
         print("[INFO] Trying to connect to %s" % ports[i])
         try:
-            serialPort = serial.Serial(ports[i], baudRate, timeout =0.5)
+            serialPort = serial.Serial(ports[i], baudRate, timeout=1)
             break
         except Exception as e:
             if "resource busy" in e.message:
@@ -128,127 +171,37 @@ def getSerialPort(portPath, baudRate = 9600, timeout = 1):
             else:
                 pass
 
-    if not serialPort:
+    if serialPort is None:
         print("[ERROR] I could not connect. Tried: %s" % ports)
         quit()
 
     print("[INFO] Connected to %s" % serialPort)
     return serialPort
 
-def writeTrialData(serialPort, saveDirec, trialsDict = {}):
-    runningTrial, csType = serialPort.readline().split()
-    while serialPort.readline() != DATA_BEGIN_MARKER:
-        continue
-
-    while True:
-        line = serialPort.readline()
-        if line is DATA_END_MARKER:
-            break
-        else:
-            timeStamp, blinkValue = line.split()
-            trialsDict[runningTrial].append((timeStamp, blinkValue))
-
-    outfile = os.path.join(saveDirec, "Trial" + runningTrial + ".csv")
-    print("[INFO] Writing to %s" % outfile)
-    with open(outfile, 'w') as f:
-        f.writeline("# 3rd row values are trial index, cs type.")
-        f.writeline("# Actual trial data starts from row 4")
-        line = runningTrial + "," + csType
-        f.writeline(line)
-        data = [ timeStamp + "," + blinkValue
-                for (timeStamp, blinkValue) in trialsDict[runningTrial]]
-        f.write("\n".join(trialsDict[runningTrial]))
-        print("++ Wrote %s, %s" % (line, data))
-
-
-def writeProfilingData(serialPort, saveDirec, profilingDict = {}):
-    while serialPort.readline() != DATA_BEGIN_MARKER:
-        continue
-    while True:
-        line = serialPort.readline()
-        print("Line is: %s" % line)
-        if line is DATA_END_MARKER:
-            break
-        else:
-            bin, counts = line.split()
-            profilingDict[bin] = counts
-
-    with open(os.path.join(saveDirec, "profilingData.csv"), 'w') as f:
-        data = profilingDict.items()
-        data = [bin + "," + count for (bin, count) in data]
-        f.write("\n".join(data))
-
-def writeData(serialPort, saveDirec, trialsDict, profilingDict):
-    operationMap = { TRIAL_DATA_MARKER : lambda port, direc: writeTrialData(port, direc, trialsDict)
-        , PROFILING_DATA_MARKER : lambda port, direc: writeProfilingData(port, direc, profilingDict)
-        }
-    print("A")
-    print("AA: %s" % serialPort.readline())
-    arduinoData = serialPort.readline()
-    while not arduinoData.strip():
-        print("[WARN] Nothing is read from serial port. Waiting for data ... ")
-        time.sleep(0.1)
-        arduinoData = serialPort.readline()
-
-    # FIXME: Don't know what it does.
-    #while serialPort.readline() != START_OF_SESSION_MARKER:
-    #    continue
-
-    while True:
-        arduinoData = serialPort.readline()
-        print("C: %s" % arduinoData)
-        if END_OF_SESSION_MARKER == arduinoData:
-            return
-        elif arduinoData.startswith(COMMENT_MARKER):
-            print("[DEBUG] Inside writeData: %s" % arduinoData)
-            operationMap[arduinoData](serialPort, saveDirec)
-        else:
-            print("B %s" % arduinoData)
-
 def process_data(port, **kwargs):
-    global args_
-    global exp_
+    global args_, exp_
     while True:
         line = port.readline().strip()
-        if not line:
-            continue
-        mousePat = re.compile(r'Mouse(?P<name>\w\d)')
-        sesPat = re.compile(r'Session(?P<sno>\d+)\:\s*(?P<stype>(Cntrl|Delay|Trace))')
-        if exp_.current_state() == "WAITING_FOR_MOUSE_INFO":
-            m = mousePat.search(line)
-            if m:
-                exp_.mouseName = m.group('name')
-                inform_user("Got mouse : %s" % exp_.mouseName)
-        elif exp_.current_state() == "WAITING_FOR_SESSION_INFO":
-            m = sesPat.search(line)
-            if m:
-                exp_.session = [m.group('sno'), m.group('stype')]
-                inform_user("Session: %s" % exp_.session)
-        elif exp_.current_state() == "WAITING_FOR_DATA":
-            print(line)
-        else:
-            print("[TODO] Fix me here")
-
-
+        exp_.insert_line(line)
 
 def write_data( serial_port):
     """Main function responsible for writing data.
     """
     global args__
-    line = serial_port.readline()
     userInformed = False
     while True:
         line = serial_port.readline()
+        write_raw_data(line)
         if not line.strip():
             if not userInformed:
                 userInformed = True
                 msg = ["Waiting for trial to start.", "Press SELECT on board" ]
                 inform_user(msg)
+                break
             else:
                 print(".", end='')
                 sys.stdout.flush()
-        else:
-            process_data(serial_port)
+    process_data(serial_port) 
 
 def init_storage():
     """
@@ -260,16 +213,26 @@ def init_storage():
     stamp = datetime.datetime.now().strftime("%Y%m%d")
     dataDir = os.path.join(outdir, stamp)
     if not os.path.isdir(dataDir):
-        _logger.info("Creating directory %s" % dataDir)
+        inform_user("Creating directory %s" % dataDir)
         os.makedirs(dataDir)
     else:
         inform_user("Directory %s already exists. Using it" % dataDir)
     args_['data_dir'] = dataDir
+    # Create a file with stamp.
+    args_['raw_data_file'] = os.path.join(dataDir, 'raw_data.txt')
+    with open(args_['raw_data_file'], "w") as f:
+        f.write("# raw_data: %s\n" % datetime.datetime.now().isoformat())
+
+def write_raw_data(line):
+    global args_
+    with open(args_['raw_data_file'], "a") as f:
+        f.write("%s\n" % line)
 
 def main():
     global args_
+    global raw_data_
     init_storage()
-    serialPort = getSerialPort( args_['port'] )
+    serialPort = get_serial_port( args_['port'] )
     try:
         write_data(serialPort)
     except KeyboardInterrupt as e:
---
+++ python/run.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./eyeBlinkDataSave.py -n 2 -st 1 -sn 1
